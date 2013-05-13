bdf85d86c85875599a223db686fc304e055e4a44
notdryft <gui.corre@gmail.com>
2013-05-13 14:51:04 +0200

    Project base : maven module, config files, some empty classes

---
+++ src/main/java/com/formation/spring/dao/BookDao.java
@@ -0,0 +1,14 @@
+package com.formation.spring.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Guillaume Corré
+ * Date: 13/05/13
+ * Time: 14:44
+ */
+@Repository
+public class BookDao {
+
+}
---
+++ src/main/java/com/formation/spring/services/LibraryService.java
@@ -0,0 +1,14 @@
+package com.formation.spring.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Guillaume Corré
+ * Date: 13/05/13
+ * Time: 14:44
+ */
+@Service
+public class LibraryService {
+
+}
