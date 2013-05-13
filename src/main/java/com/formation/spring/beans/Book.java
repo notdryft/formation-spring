0955979c559fd431f79de8957390858f0d3a83c6
notdryft <gui.corre@gmail.com>
2013-05-13 14:51:04 +0200

    Project base : maven module, config file, some empty classes

---
+++ src/main/java/com/formation/spring/beans/Book.java
@@ -0,0 +1,14 @@
+package com.formation.spring.beans;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Guillaume Corré
+ * Date: 13/05/13
+ * Time: 14:43
+ */
+@Component
+public class Book {
+
+}
